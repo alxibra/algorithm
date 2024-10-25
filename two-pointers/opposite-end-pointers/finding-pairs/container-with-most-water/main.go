510305595c80cb49ebc580d83be2053842dea778
alxibra <alxibra@forstok.com>
2024-10-25 09:13:09 +0700

    brute force for container with most water

---
+++ two-pointers/opposite-end-pointers/finding-pairs/container-with-most-water/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	result := bruteForce(height)
+
+	fmt.Println("result: ", result)
+}
+
+func bruteForce(height []int) int {
+	maxCapacity := math.MinInt32
+	for i := 0; i < len(height)-1; i++ {
+		for j := i + 1; j < len(height); j++ {
+			currentCapacity := math.Min(float64(height[i]), float64(height[j])) * float64(j-i)
+
+			if currentCapacity > float64(maxCapacity) {
+				maxCapacity = int(currentCapacity)
+			}
+		}
+		fmt.Println("max capacity: ", maxCapacity)
+	}
+	return maxCapacity
+}
