f30cfbeaf4b2f9b387226ab06ecb536de76c4753
alxibra <alxibra@forstok.com>
2024-10-27 15:50:38 +0700

    add min heap solution

--- two-pointers/opposite-end-pointers/maximum-sum/main.go
+++ two-pointers/opposite-end-pointers/maximum-sum/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"math"
 )
@@ -9,6 +10,58 @@ func main() {
 	nums := []int{1, 6, 3, 5, 7}
 	bruteForce(nums)
 	singlePass(nums)
+	twoPass(nums)
+	Heap(nums)
+}
+
+func twoPass(nums []int) {
+	maxFirst := math.MinInt32
+
+	for _, v := range nums {
+		if v > maxFirst {
+			maxFirst = v
+		}
+	}
+
+	maxSecond := math.MinInt32
+	for _, num := range nums {
+		if num > maxSecond && num < maxFirst {
+			maxSecond = num
+		}
+	}
+	sum := maxFirst + maxSecond
+	fmt.Println("max: ", sum)
+}
+
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *MinHeap) Push(num any) {
+	*h = append(*h, num.(int))
+}
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+func Heap(nums []int) {
+	h := &MinHeap{}
+	heap.Init(h)
+	for index := 0; index < 2; index++ {
+		h.Push(nums[index])
+	}
+
+	for i := 2; i < len(nums); i++ {
+		if nums[i] > (*h)[0] {
+			heap.Pop(h)
+			heap.Push(h, nums[i])
+		}
+	}
+	fmt.Println("heap: ", h)
 }
 
 func singlePass(nums []int) {
