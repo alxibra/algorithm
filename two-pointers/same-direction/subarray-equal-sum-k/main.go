a7fd63a28a3dd4c4ef0989751f8c2e2b1c3ebe89
alxibra <alxibra@forstok.com>
2024-11-01 17:24:26 +0700

    use prefix sum

--- two-pointers/same-direction/subarray-equal-sum-k/main.go
+++ two-pointers/same-direction/subarray-equal-sum-k/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 1, 1}
+	nums := []int{1, 1, 1, 2}
 	k := 2
-	fmt.Println("count: ", bruteForce(nums, k))
+	// fmt.Println("count: ", bruteForce(nums, k))
+	fmt.Println("count: ", bruteForcePrefixSum(nums, k))
 }
 
 func bruteForce(nums []int, k int) int {
@@ -18,6 +19,32 @@ func bruteForce(nums []int, k int) int {
 				count++
 			}
 		}
+	}
+	return count
+}
+
+func bruteForcePrefixSum(nums []int, k int) int {
+	n := len(nums)
+	count := 0
+
+	prefix := make([]int, n)
+	prefix[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		prefix[i] = prefix[i-1] + nums[i]
+	}
+
+	for start := 0; start < n; start++ {
+		for end := start; end < n; end++ {
+			sum := prefix[end]
+			if start > 0 {
+				sum -= prefix[start-1]
+			}
+			// Check if the sum of this subarray is equal to k
+			if sum == k {
+				count++
+			}
+		}
+		fmt.Println("-----")
 	}
 	return count
 }
