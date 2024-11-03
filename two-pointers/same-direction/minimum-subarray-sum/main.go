b51a45a03b334825190a1a68d08536515098b48c
alxibra <alxibra@forstok.com>
2024-11-03 12:12:10 +0700

    brute force

--- two-pointers/same-direction/minimum-subarray-sum/main.go
+++ two-pointers/same-direction/minimum-subarray-sum/main.go
@@ -32,9 +32,29 @@ func min(a, b int) int {
 	return b
 }
 
+func bruteForce(k int, nums []int) int {
+	minL := math.MaxInt32
+
+	for right := 0; right < len(nums); right++ {
+		sum := 0
+		for left := right; left < len(nums); left++ {
+			sum += nums[left]
+			if sum >= k {
+				minL = min(minL, left-right+1)
+				break
+			}
+		}
+	}
+	if minL == math.MaxInt32 {
+		minL = 0
+	}
+	return minL
+}
+
 func main() {
 	// Test case
 	target := 7
 	nums := []int{2, 3, 1, 2, 4, 3}
-	fmt.Println("Minimum subarray length:", minSubArrayLen(target, nums)) // Output: 2
+	// fmt.Println("Minimum subarray length:", minSubArrayLen(target, nums)) // Output: 2
+	fmt.Println("Minimum subarray length:", bruteForce(target, nums)) // Output: 2
 }
